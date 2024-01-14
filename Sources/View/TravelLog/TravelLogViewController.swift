0b19bcab59a1d45157d8279f8e37902e9a5d8a7e
dogyungkim <prunsoli12@gmail.com>
2024-01-14 22:46:59 +0900

    FEAT:#3

--- Sources/View/TravelLog/TravelLogViewController.swift
+++ Sources/View/TravelLog/TravelLogViewController.swift
@@ -1,17 +1,17 @@
 //
-//  ViewController.swift
+//  TravelLogViewController.swift
 //
 //  Created by 김도경 on 1/8/24.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
+class TravelLogViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .systemGray
     }
 }
 
