2d25051a98369608d73491fb86c78927b30feffc
Do Gyung Kim (Bryan) <91521677+dogyungkim@users.noreply.github.com>
2024-01-15 10:10:38 +0900

    Revert "FEAT:#3"

--- Sources/Main/MainViewController.swift
+++ Sources/Main/MainViewController.swift
@@ -1,17 +1,17 @@
 //
-//  TravelLogViewController.swift
+//  ViewController.swift
 //
 //  Created by 김도경 on 1/8/24.
 //
 
 import UIKit
 
-class TravelLogViewController: UIViewController {
+class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGray
+        view.backgroundColor = .systemCyan
     }
 }
 
--- Sources/SceneDelegate.swift
+++ Sources/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = .init(windowScene: windowScene)
-        window?.rootViewController = TabBarViewController()
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
     }
 
