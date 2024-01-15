c2dc5ce1bc9624ffb1cbb50fc87502496529e5d7
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-01-15 17:24:46 +0900

    [FEAT] OnBoardingView 추가

---
+++ Sources/View/OnBoarding/LoginView.swift
@@ -0,0 +1,92 @@
+//
+//  LoginView.swift
+//  WithYou
+//
+//  Created by 이승진 on 2024/01/15.
+//  Copyright © 2024 withyou.org. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class LoginView: UIView {
+    
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "MainLogo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let appleLoginButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "AppleLogin"), for: .normal)
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    let googleLoginButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "GoogleLogin"), for: .normal)
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    let kakaoLoginButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "KakaoLogin"), for: .normal)
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+//        backgroundColor = .white
+        backgroundColor = UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha: 1)
+        
+        addSubview(logoImageView)
+        addSubview(appleLoginButton)
+        addSubview(googleLoginButton)
+        addSubview(kakaoLoginButton)
+    
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setConstraints() {
+        
+        logoImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-20)
+        }
+        
+        appleLoginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(logoImageView.snp.bottom).offset(80)
+            make.width.equalTo(360)
+            make.height.equalTo(60)
+        }
+        
+        googleLoginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(appleLoginButton.snp.bottom).offset(10)
+            make.width.equalTo(360)
+            make.height.equalTo(60)
+        }
+        
+        kakaoLoginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(googleLoginButton.snp.bottom).offset(10)
+            make.width.equalTo(360)
+            make.height.equalTo(60)
+        }
+    }
+    
+    @objc private func loginButtonTapped() {
+        
+    }
+}
