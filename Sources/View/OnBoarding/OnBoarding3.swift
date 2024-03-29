8ee1844d43eb6afb6b86321607079795b70b4afb
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-03-29 14:23:22 +0900

    refactor: OnBoarding3

--- Sources/View/OnBoarding/OnBoarding3.swift
+++ Sources/View/OnBoarding/OnBoarding3.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SnapKit
 
-final class OnBoarding3: UIView {
+final class OnBoarding3: UIView, BaseViewProtocol {
     
     let mainLabel: UILabel = {
         let label = UILabel()
@@ -42,20 +42,26 @@ final class OnBoarding3: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-//        backgroundColor = .white
-        backgroundColor = UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha: 1)
-        addSubview(mainLabel)
-        addSubview(subLabel)
-        addSubview(mockUpImageView)
-    
-        setConstraints()
+        setUpViewProperty()
+        setUp()
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    private func setConstraints() {
+    
+    func setUpViewProperty() {
+        backgroundColor = UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha: 1)
+    }
+    
+    func setUp() {
+        addSubview(mainLabel)
+        addSubview(subLabel)
+        addSubview(mockUpImageView)
+    }
+    
+    func setLayout() {
         mainLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(subLabel.snp.top).offset(-10)
@@ -71,4 +77,9 @@ final class OnBoarding3: UIView {
             make.bottom.equalToSuperview()
         }
     }
+    
+    func setDelegate() {
+        // .
+    }
+
 }
