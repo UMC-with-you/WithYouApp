3156187a6bd76ef182d9573d3356358f675de9d5
bae-suho <sugh6019@naver.com>
2025-04-22 18:13:30 +0900

    Fix: ColorSlider에서 ThumbView 클래스 파일 분리

--- Sources/CommonUI/UIComponents/ColorSlider/ColorSlider.swift
+++ Sources/CommonUI/UIComponents/ColorSlider/ColorSlider.swift
@@ -165,38 +165,3 @@ final class ColorSlider: UISlider {
         configure()
     }
 }
-
-// MARK: - ThumbView
-
-final class ThumbView: UIView {
-    let centerDot = UIView()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setup()
-    }
-
-    /// Thumb view의 설정을 초기화하는 메서드
-    private func setup() {
-        let dotSize = CGFloat(40).adjusted
-        centerDot.frame = CGRect(x: bounds.midX - dotSize / 2,
-                                 y: bounds.midY - dotSize / 2,
-                                 width: dotSize,
-                                 height: dotSize)
-        centerDot.backgroundColor = .white
-        centerDot.layer.cornerRadius = dotSize / 2
-        centerDot.isUserInteractionEnabled = false
-        
-        addSubview(centerDot)
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        centerDot.center = CGPoint(x: bounds.midX, y: bounds.midY)
-    }
-}
